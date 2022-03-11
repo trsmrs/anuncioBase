fc5bd01191e46009b3652005fcf254d5aae0b6b5
Tiago <trsmtiago@gmail.com>
2022-03-11 11:31:30 -0300

    adicionado pagina publish

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,4 @@
+import { green } from '@material-ui/core/colors'
 import { createTheme } from '@material-ui/core/styles'
 
 
@@ -5,8 +6,15 @@ const theme = createTheme({
 palette:{
     primary:{
         main: '#000000',
-    }
+    
+  },
+  background:{
+    default: 'rgb(245, 244, 245)',
+    white: '#ffffff'
+    
   }
+},
+
 })
 
 export default theme
