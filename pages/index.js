ced429a2703adacd726a9b39f80b3ffcab4e4125
Tiago <trsmtiago@gmail.com>
2022-03-14 13:44:04 -0300

    header- adicionado menu usuario

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ const Home = () => {
                     </IconButton>
                 </Paper>
             </Container>
-            <Container maxWidth="md" className={classes.cardGrid}>
+            <Container maxWidth="lg" className={classes.cardGrid}>
                 <Typography component ='h2' variant='h4' align='center' color='textPrimary'>
                     Destaques
                 </Typography>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Link from 'next/link'
+import {AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Container,
+  Avatar,
+  MenuItem,
+  Divider,
+  Menu,
+  
+
+       } from '@material-ui/core';
+import {AccountCircle, MenuIcon} from '@material-ui/icons';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,24 +27,68 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  nameIcon:{
+    margin: ' 0 10px',
+  },
+  divider:{
+    marginTop: 4,
+  }
 }));
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const [anchorUserMenu, setAnchorUserMenu] = useState(false)
+
+  const openUserMenu = Boolean(anchorUserMenu)
+
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
+    <>
+      <AppBar position="static" elevation={3}>
+       <Container maxWidth='lg'>
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            Anuncios CloneOL
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Link href={'/user/publish'} passHref>
+          <Button color="inherit" variant="outlined">
+            Anunciar e Vender
+          </Button>
+          </Link>
+          <IconButton color="secondary" onClick={(e)=>{setAnchorUserMenu(e.currentTarget)}}>
+            {
+              true === true
+             ? <Avatar src=""/>
+             : <AccountCircle />
+            }
+          <Typography variant="subtitle2" color="secondary" className={classes.nameIcon}>
+            Tiago Machado
+          </Typography>
+          </IconButton>
+
+          <Menu anchorEl={anchorUserMenu}
+           open={anchorUserMenu}
+           onClose={()=>
+             setAnchorUserMenu(null)}
+             anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+             }}
+           
+          >   
+           <Link href="/user/dashboard" passHref> 
+            <MenuItem>Meus anúncios</MenuItem>
+            </Link>
+            <Link href="/user/publish" passHref>
+            <MenuItem>Publicar novos anúncios</MenuItem>
+            </Link>
+            <Divider className={classes.divider}/>
+            <MenuItem>Sair</MenuItem>
+
+          </Menu>
         </Toolbar>
+        </Container>
       </AppBar>
-    </div>
+    </>
   );
 }
